1a36b29a705654d9d8f4c06a863a17d7be932726
rubenfsgomes <ruben_fsgomes@hotmail.com>
2024-09-12 16:54:37 +0100

    End of section 9

---
+++ frontend/web-app/types/next-auth.d.ts
@@ -0,0 +1,23 @@
+import { type DefaultSession } from "next-auth"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import {JWT} from 'next-auth/jwt';
+ 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      username: string
+    } & DefaultSession["user"]
+    accessToken: string
+  }
+
+  interface Profile {
+    username: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    username: string
+    accessToken: string
+  }
+}
